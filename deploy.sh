a5275cf3cf7ae81c1e97d16ea8ee44cfd279d846
Disha P Khanted <43805337+dishakhanted@users.noreply.github.com>
2023-12-22 01:35:07 -0600

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+docker kill columni_users 2> /dev/null || true
+docker system prune -a -f
+docker run --name columni_users -d -p 8011:8011 053467565326.dkr.ecr.us-east-1.amazonaws.com/columni:latest
